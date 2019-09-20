b0f76acfceb6e03d58b1959d18f00bfd18beab4d
Trevor Osterman <trevormosterman@gmail.com>
2019-09-19 20:47:47 -0700

    2 hour mark hit, missing /delete functionality

--- src/app.js
+++ src/app.js
@@ -5,14 +5,18 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const winston = require("winston");
+const uuid = require("uuid/v4");
 
 const app = express();
 
+app.use(morgan("dev"));
+
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(express.json());
 
 const logger = winston.createLogger({
   level: "info",
@@ -30,7 +34,6 @@ if (NODE_ENV !== "production") {
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  console.log(apiToken);
   const authToken = req.get("Authorization");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
@@ -43,6 +46,90 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get("/", (req, res) => {
   res.send("Hello, boilerplate!");
+});
+
+const bookmarks = [
+  {
+    id: 1,
+    title: "google",
+    url: "www.google.com",
+    rating: 4
+  },
+  {
+    id: 2,
+    title: "yahoo",
+    url: "www.yahoo.com",
+    rating: 3
+  },
+  {
+    id: 3,
+    title: "npr",
+    url: "www.npr.org",
+    rating: 5
+  }
+];
+
+app.get("/bookmarks", (req, res) => {
+  res.json(bookmarks);
+});
+
+app.get("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+  const bookmark = bookmarks.find(b => b.id == id);
+
+  if (!bookmark) {
+    logger.error(`Bookmark with id ${id} not found.`);
+    return res.status(404).send("Bookmark Not Found");
+  }
+
+  res.json(bookmark);
+});
+
+app.post("/bookmarks", (req, res) => {
+  const { title, url, rating } = req.body;
+
+  if (!title) {
+    logger.error(`Title is required`);
+    return res.status(400).send("Invalid data");
+  }
+
+  if (!url) {
+    logger.error(`URL is required`);
+    return res.status(400).send("Invalid data");
+  }
+
+  if (!rating) {
+    logger.error(`Rating is required`);
+    return res.status(400).send("Invalid data");
+  }
+
+  const id = uuid();
+  bookmark = { id, title, url, rating };
+  bookmarks.push(bookmark);
+
+  logger.info(`Bookmark with id ${id} created`);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/card/${id}`)
+    .json(bookmark);
+});
+
+app.delete("./bookmarks/:bmid", (req, res) => {
+  const { bmid } = req.params;
+  const index = bookmarks.findIndex(b => b.id == bmid);
+
+  if (index === -1) {
+    return res.status(404).send("Bookmark not found");
+  }
+
+  bookmarks.splice(index, 1);
+
+  logger.info(`Bookmark with id ${bmid} deleted`);
+  res
+    .status(204)
+    .json(bookmarks)
+    .end();
 });
 
 app.use(function errorHandler(error, req, res, next) {
